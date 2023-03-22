4ad69507c52660105e0ceb75171fe7bb90935bb1
NkgodisengM <nkgodisengmmethi@gmail.com>
2023-03-21 22:25:28 -0700

    API intergration
    Updated the css with a text transform capitalize, added axios to fetch weather API and select temperature, city, humidity and wind to replace with current weather.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-function showTemp(response) {
-  let apiKey = "05c98b0bebf349dd65cfftbab836o4c9";
-  let city = "Polokwane";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=Polokwane&key=05c98b0bebf349dd65cfftbab836o4c9&units=metric";
-  axios.get(apiUrl).then(showTemp);
+function displayTemperature(response) {
+  console.log(apiUrl);
+  console.log(response);
+
+  let temparatureElement = document.querySelector("#temperature-value");
+  temparatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector(".header");
+  cityElement.innerHTML = response.data.city;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let speed = document.querySelector("#speed");
+  speed.innerHTML = response.data.wind.speed;
 }
-console.log(apiUrl);
+let apiKey = "05c98b0bebf349dd65cfftbab836o4c9";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=New York&key=05c98b0bebf349dd65cfftbab836o4c9&units=metric";
+axios.get(apiUrl).then(displayTemperature);
