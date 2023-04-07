32c23108d128b771e1c0cef01812ef53acf12f52
NkgodisengM <nkgodisengmmethi@gmail.com>
2023-04-06 23:11:18 -0700

    Created the JS template from the HTML for the forecas
    Removed the HTML to inject inside js, created a new function called displayForecast, created an array of different days, each time the function will loop through each day to display its forecast inside the ForecastElement selected.

--- src/index.js
+++ src/index.js
@@ -65,3 +65,29 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="" width="42">
+              <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max"> 18° </span> 
+              <span class="weather-forecast-temperature-min"> 14°</span>
+              </div>
+            </div>
+          `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
